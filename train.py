3c521a7efb0df4610c92951dbb46d05998a700e4
Luca <ngtruongthanh98@gmail.com>
2024-10-21 20:09:55 +0700

    Refactor train.py to save src_input as a txt file during evaluation

--- train.py
+++ train.py
@@ -341,9 +341,9 @@ def evaluate(args, config, dev_dataloader, model, tokenizer, epoch, beam_size=1,
                     tts_ref.save(ref_path)
 
                     # save src_input as a txt file
-                    src_input_path = os.path.join(sample_dir, f'src_input_{temp_name}.txt')
+                    src_input_path = os.path.join(sample_dir, f'src_input.txt')
                     with open(src_input_path, 'w') as src_input_file:
-                        src_input_file.write(f"src_input: {src_input}\n")
+                        src_input_file.write(src_input)
 
                     # Create a text file to store txt_hyp and txt_ref
                     text_file_path = os.path.join(sample_dir, f'{temp_name}.txt')
