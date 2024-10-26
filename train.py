e69d098d1b6b7e668f6cf60a10888d45520ac573
Luca <ngtruongthanh98@gmail.com>
2024-10-26 20:16:43 +0700

    Refactor train.py: Update file loading to use YAML format for keypoints data

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def get_args_parser():
     parser.add_argument("--project", type=str, default='VLP',
                         help="wandb project",
                         )
-    passet.add_argument("--input-keypoints-path", type=str, default='../17February_2011_Thursday_heute-387/src_input.pkl',
+    parser.add_argument("--input-keypoints-path", type=str, default='../17February_2011_Thursday_heute-387/src_input.pkl',
                         help="path to the input keypoints file",
                         )
 
